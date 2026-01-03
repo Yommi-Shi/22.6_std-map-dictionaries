0b09a290f2475aac89415556de2bd2b454323ac7
Evgenii Blinov <scythian89@gmail.com>
2026-01-03 13:49:40 +0300

    function fix - phone_number_by_last_name

--- 01_task/main.cpp
+++ 01_task/main.cpp
@@ -12,60 +12,63 @@ void title() {
     std::cout << "->";
 }
 
-void add_subscriber(std::map<std::string, std::string> &phone_direct) {
+void add_subscriber(std::map<std::string, std::string> &number_to_surname,
+                    std::map<std::string, std::vector<std::string> > &surname_to_number) {
     std::cout << "Enter your phone number and last name in the format 69-70-30 Ivanov" << std::endl;
     std::string phone, last_name;
     std::cin >> phone >> last_name;
-    phone_direct.insert(std::make_pair(phone, last_name));
+    number_to_surname.insert(std::make_pair(phone, last_name));
+    if (surname_to_number.contains(last_name)) {
+        surname_to_number[last_name].push_back(phone);
+    } else {
+        surname_to_number.insert(std::make_pair(last_name, std::vector<std::string>{phone}));
+    }
 }
 
-void last_name_by_phone_number(std::map<std::string, std::string> &phone_direct) {
+void last_name_by_phone_number(std::map<std::string, std::string> &number_to_surname) {
     std::cout << "Enter a phone number to search for a last name:";
     std::string phone;
     std::cin >> phone;
-    std::map<std::string, std::string>::iterator it = phone_direct.find(phone);
-    if (it != phone_direct.end()) {
+    std::map<std::string, std::string>::iterator it = number_to_surname.find(phone);
+    if (it != number_to_surname.end()) {
         std::cout << "Phone number owner " << it->first << " -> " << it->second << std::endl;
     } else {
         std::cout << "This phone number is not in the directory" << std::endl;
     }
 }
 
-void phone_number_by_last_name(std::map<std::string, std::string> &phone_direct) {
-    std::vector<std::string> list_name;
+void phone_number_by_last_name(std::map<std::string, std::vector<std::string>> &surname_to_number) {
     std::cout << "Enter last name to search for phone number:";
     std::string last_name;
     std::cin >> last_name;
-    for (std::map<std::string, std::string>::iterator it = phone_direct.begin(); it != phone_direct.end(); ++it) {
-        if (it->second == last_name) {
-            list_name.push_back(it->first);
-        }
-    }
-    if (list_name.empty()) {
-        std::cout << "This last name is not in the directory" << std::endl;
-    } else {
+
+    std::map<std::string, std::vector<std::string>>::iterator it = surname_to_number.find(last_name);
+    if (it != surname_to_number.end()) {
         std::cout << "Subscribers have this phone number: ";
-        for (std::string name: list_name) {
-            std::cout << name << " ";
+        for (std::string str: it->second) {
+            std::cout << str << " ";
         }
         std::cout << std::endl;
+    } else {
+        std::cout << "This last name is not in the directory" << std::endl;
     }
 }
 
 int main() {
     std::cout << "===Telephone directory===" << std::endl;
 
-    std::map<std::string, std::string> phone_direct;
+    std::map<std::string, std::string> number_to_surname;
+    std::map<std::string, std::vector<std::string>> surname_to_number;
 
     title();
     std::string cmd;
     while (std::cin >> cmd && cmd != "q") {
         if (cmd == "1") {
-            add_subscriber(phone_direct);
+            add_subscriber(number_to_surname, surname_to_number);
         } else if (cmd == "2") {
-            last_name_by_phone_number(phone_direct);
+            last_name_by_phone_number(number_to_surname);
         } else if (cmd == "3") {
-            phone_number_by_last_name(phone_direct);
+            phone_number_by_last_name(surname_to_number);
         } else {
             std::cerr << "Invalid input" << std::endl;
         }
